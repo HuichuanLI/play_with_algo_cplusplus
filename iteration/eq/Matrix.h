2ff68437ab376ab95d5a7541618610dd7ee104b8
huichuan <lhc14124908@163.com>
2021-11-23 19:21:41 +0800

    add

---
+++ iteration/eq/Matrix.h
@@ -0,0 +1,44 @@
+//
+// Created by 李汇川 on 2021/11/23.
+//
+
+#ifndef PLAY_WITH_ALGO_ITERATION_EQ_MATRIX_H_
+#define PLAY_WITH_ALGO_ITERATION_EQ_MATRIX_H_
+#include <vector>
+
+template<typename T>
+class CMatrix {
+ public:
+  explicit CMatrix() : m_array(0) {}
+
+  CMatrix(int rows, int cols) : m_array(rows) {
+    for (int i = 0; i < rows; ++i) {
+      m_array[i].resize(cols);
+    }
+  }
+
+  CMatrix(T *m, int rows, int cols) : m_array(rows) {
+    for (int i = 0; i < rows; ++i) {
+      m_array[i].assign(&m[i * cols], &m[i * cols + cols]);
+    }
+  }
+
+  CMatrix(const CMatrix<T> &m) { *this = m; }
+
+  void resize(int rows, int cols);           // 改变当前矩阵大小
+  void swap_row(int row1, int row2);         // 将换两行的数据
+
+  int rows() const { return m_array.size(); }
+  int cols() const { return rows() ? (m_array[0].size()) : 0; }
+  bool empty() const { return rows() == 0; }        // 是否为空
+
+
+  const std::vector<T> &operator[](int row) const { return m_array[row]; } //[]操作符重载
+  std::vector<T> &operator[](int row) { return m_array[row]; }
+
+ protected:
+  std::vector<std::vector<T>> m_array;
+};
+
+
+#endif //PLAY_WITH_ALGO_ITERATION_EQ_MATRIX_H_
---
+++ iteration/eq/jacobi.h
@@ -0,0 +1,10 @@
+//
+// Created by 李汇川 on 2021/11/23.
+//
+
+#ifndef PLAY_WITH_ALGO_ITERATION_EQ_JACOBI_H_
+#define PLAY_WITH_ALGO_ITERATION_EQ_JACOBI_H_
+
+
+
+#endif //PLAY_WITH_ALGO_ITERATION_EQ_JACOBI_H_
