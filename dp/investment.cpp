8aea5a7d786ef17213d88385043d40883e483c23
huichuan <lhc14124908@163.com>
2022-01-03 23:57:47 +0800

    add

--- dp/investment.cpp
+++ dp/investment.cpp
@@ -14,45 +14,39 @@ int f[MAX_N][MAX_M] =
         {0, 75, 100, 120, 135, 150, 180}
     };
 
-void print_result(int n, int m, int max[][MAX_M])
-{
-  int invesment[MAX_N] = { 0 };
+void print_result(int n, int m, int max[][MAX_M]) {
+  int invesment[MAX_N] = {0};
 
   invesment[n - 1] = max[n - 1][m];
-  for (int i = n - 2; i >= 0; --i)
-  {
+  for (int i = n - 2; i >= 0; --i) {
     int t = 0;
-    for (int j = n - 1; j > i; --j)
-    {
+    for (int j = n - 1; j > i; --j) {
       t += invesment[j];
     }
     invesment[i] = max[i][m - t];
   }
 
-  for (int x = 0; x < n; x++)
-  {
+  for (int x = 0; x < n; x++) {
     printf("Invest %d for project %d\n", invesment[x], x + 1);
   }
 }
 
 // n个项目,投资m百万元
-int investment(int n, int m, int d[][MAX_M], int max[][MAX_M])
-{
+int investment(int n, int m, int d[][MAX_M], int max[][MAX_M]) {
   //初始化（也就是只有一个项目的情况）
-  for (int x = 0; x <= m; x++)
-  {
+  for (int x = 0; x <= m; x++) {
     d[0][x] = f[0][x];  // 投资第一个项目
     max[0][x] = x; //标记函数初始化
   }
 
-  for(int i = 1; i < n; i++) // 投资前i个项目
+  for (int i = 1; i < n; i++) // 投资前i个项目
   {
-    for(int j = 0; j <= m; j++) // 前 i 个项目总投入的钱数 j
+    for (int j = 0; j <= m; j++) // 前 i 个项目总投入的钱数 j
     {
-      for(int k = 0; k <= j; k++) // 投资当前项目的钱数（第 i 个项目）
+      for (int k = 0; k <= j; k++) // 投资当前项目的钱数（第 i 个项目）
       {
-        int tmp = f[i][k] + d[i - 1][j-k];
-        if(tmp > d[i][j]) //d[i][j]初始化都是0
+        int tmp = f[i][k] + d[i - 1][j - k];
+        if (tmp > d[i][j]) //d[i][j]初始化都是0
         {
           d[i][j] = tmp; // 更新当前的最优解
           max[i][j] = k; // 更新标记函数
@@ -64,14 +58,17 @@ int investment(int n, int m, int d[][MAX_M], int max[][MAX_M])
   return d[n - 1][m];
 }
 
-
-int main()
-{
-  int d[MAX_N][MAX_M] = { { 0 } }; // 投资前 i 个项目 j 百万的最大收益
-  int max[MAX_N][MAX_M] = { { 0 } }; // 投资决策标记值
-
+int main() {
+  int d[MAX_N][MAX_M] = {{0}}; // 投资前 i 个项目 j 百万的最大收益
+  int max[MAX_N][MAX_M] = {{0}}; // 投资决策标记值
   int benefit = investment(3, 6, d, max);
   printf("Total benefit : %d\n", benefit);
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 6; j++) {
+      printf("max[%d][%d]:%d\n", i,j,max[i][j]);
+    }
+  }
   print_result(3, 6, max); // 打印结果
   return 0;
 }
