671f7ccf89346cbf7e96247de2dad583e9480dde
huichuan <lhc14124908@163.com>
2022-03-19 16:17:18 +0800

    add

---
+++ Queue/Queue.h
@@ -0,0 +1,21 @@
+//
+// Created by 李汇川 on 2022/3/19.
+//
+
+#ifndef DYNAMIC_ARRAY_QUEUE_QUEUE_H_
+#define DYNAMIC_ARRAY_QUEUE_QUEUE_H_
+template<typename T>
+class Queue {
+ public:
+  virtual int getSize() = 0;
+
+  virtual bool isEmpty() = 0;
+
+  virtual void enqueue(T e) = 0;
+
+  virtual T dequeue() = 0;
+
+  virtual T getFront() = 0;
+};
+
+#endif //DYNAMIC_ARRAY_QUEUE_QUEUE_H_
---
+++ Queue/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 李汇川 on 2022/3/19.
+//
+
+#include <iostream>
+#include "ArrayQueue.h"
+
+int main() {
+  ArrayQueue<int> *arrayQueue = new ArrayQueue<int>();
+  for (int i = 0; i < 10; ++i) {
+    arrayQueue->enqueue(i);
+    arrayQueue->print();
+    if (i % 3 == 2) {
+      arrayQueue->dequeue();
+      arrayQueue->print();
+    }
+  }
+  arrayQueue = nullptr;
+  delete arrayQueue;
+  return 0;
+}
