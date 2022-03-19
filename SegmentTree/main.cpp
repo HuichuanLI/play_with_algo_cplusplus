ac19243b2c7c9c4c654e26d817b3b0335abe641f
huichuan <lhc14124908@163.com>
2022-03-19 22:00:00 +0800

    add

---
+++ SegmentTree/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 李汇川 on 2022/3/19.
+//
+
+#include <iostream>
+#include "SegmentTree.h"
+
+int main() {
+  int nums[] = {-2, 0, 3, -5, 2, -1};
+
+  SegmentTree<int> *segmentTree = new SegmentTree<int>(nums, sizeof(nums) / sizeof(int), [](int a, int b) -> int {
+    return a + b;
+  });
+  std::cout << segmentTree->query(2, 5) << std::endl;
+  segmentTree->print();
+  return 0;
+}
