5ee901ffa6dd399127682eba25ce7c2acf6c8f8d
huichuan <lhc14124908@163.com>
2022-03-19 12:53:29 +0800

    add

---
+++ Array/main.cpp
@@ -0,0 +1,31 @@
+//
+// Created by 李汇川 on 2022/3/19.
+//
+
+#include <iostream>
+#include "Array.h"
+
+int main() {
+  Array<int> *array = new Array<int>();
+  for (int i = 0; i < 10; ++i) {
+    array->addLast(i);
+    array->print();
+  }
+  array->add(1, 100);
+  array->print();
+  array->addFirst(-1);
+  array->print();
+  array->remove(2);
+  array->print();
+  array->removeElement(4);
+  array->print();
+  for (int i = 0; i < 2; i++) {
+    array->removeLast();
+    array->print();
+    array->removeFirst();
+    array->print();
+  }
+  array = nullptr;
+  delete array;
+  return 0;
+}
