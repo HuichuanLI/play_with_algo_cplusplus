42a7f2b53b1f34ad76d37e2323ad8d826659ac2e
huichuan <lhc14124908@163.com>
2022-03-19 17:01:47 +0800

    add

---
+++ LinkedList/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 李汇川 on 2022/3/19.
+//
+#include <iostream>
+#include "LinkedList.h"
+
+int main() {
+  LinkedList<int> *linkedList = new LinkedList<int>();
+  for (int i = 0; i < 5; ++i) {
+    linkedList->addFirst(i);
+  }
+  linkedList->add(2, 30);
+  linkedList->print();
+  linkedList->remove(2);
+  linkedList->print();
+  linkedList->removeFirst();
+  linkedList->print();
+  linkedList->removeLast();
+  linkedList->print();
+  linkedList = nullptr;
+  delete linkedList;
+  return 0;
+}
