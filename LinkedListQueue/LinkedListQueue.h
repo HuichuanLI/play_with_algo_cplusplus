36e5f644a534edccb711f6845af4e235620898ec
huichuan <lhc14124908@163.com>
2022-03-19 17:58:13 +0800

    ad

---
+++ LinkedListQueue/LinkedListQueue.h
@@ -0,0 +1,89 @@
+//
+// Created by 李汇川 on 2022/3/19.
+//
+
+#ifndef DYNAMIC_ARRAY_LINKEDLISTQUEUE_LINKEDLISTQUEUE_H_
+#define DYNAMIC_ARRAY_LINKEDLISTQUEUE_LINKEDLISTQUEUE_H_
+
+#include "Queue.h"
+#include <cassert>
+#include <iostream>
+
+template<typename T>
+class QNode {
+ public:
+  T e;
+  QNode<T> *next;
+
+  QNode(T e, QNode<T> *next) : e(e), next(next) {}
+
+  QNode(T e) : e(e), next(nullptr) {}
+
+  QNode() : next(nullptr) {}
+};
+template<typename T>
+class LinkedListQueue : public Queue<T> {
+ public:
+  LinkedListQueue() {
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+  }
+
+  int getSize() {
+    return size;
+  }
+
+  bool isEmpty() {
+    return size == 0;
+  }
+
+  void enqueue(T e) {
+    if (tail == nullptr) {
+      tail = new QNode<T>(e);
+      head = tail;
+    } else {
+      tail->next = new QNode<T>(e);
+      tail = tail->next;
+    }
+    size++;
+  }
+
+  T dequeue() {
+    assert(!isEmpty());
+    QNode<T> *retNode = head;
+    head = head->next;
+    retNode->next = nullptr;
+    if (head == nullptr) {
+      tail = nullptr;
+    }
+    size--;
+    return retNode->e;
+  }
+
+  T getFront() {
+    assert(!isEmpty());
+    return head->e;
+  }
+
+  void print() {
+    QNode<T> *prev = head;
+    std::cout << "LinkListQueue: size = " << size << std::endl;
+    std::cout << "front ";
+    std::cout << "[";
+    while (prev != nullptr) {
+      std::cout << prev->e;
+      if (prev->next != nullptr) {
+        std::cout << ", ";
+      }
+      prev = prev->next;
+    }
+    std::cout << "] tail" << std::endl;
+  }
+
+ private:
+  QNode<T> *head;
+  QNode<T> *tail;
+  int size;
+};
+#endif //DYNAMIC_ARRAY_LINKEDLISTQUEUE_LINKEDLISTQUEUE_H_
