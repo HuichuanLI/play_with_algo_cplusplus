9f89426b506ad7d39f10ef52d6f0ecf726ed2877
huichuan <lhc14124908@163.com>
2022-06-03 21:56:45 +0800

    add

--- Hacker Rank /Day-2-3-Compound-Event-Probability/main.cpp
+++ Hacker Rank /Day-2-3-Compound-Event-Probability/main.cpp
@@ -1,3 +1,32 @@
 //
 // Created by 李汇川 on 2022/5/29.
 //
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+int gcd(int a, int b) {
+  assert(a > b && b > 0);
+  if (a < b) {
+    swap(a, b);
+  }
+  if (a % b == 0) {
+    return b;
+  }
+  return gcd(b, a % b);
+}
+
+int main() {
+
+  int total = 7 * 9 * 8;
+  int count = 0;
+  count += 4 * 5 * 4; // RRB
+  count += 4 * 4 * 4; // RBR
+  count += 3 * 5 * 4; // BRR
+
+  int g = gcd(total, count);
+  cout << count / g << "/" << total / g << endl;
+  return 0;
+}
--- Hacker Rank /Day-3-1-Conditional-Probability/main.cpp
+++ Hacker Rank /Day-3-1-Conditional-Probability/main.cpp
@@ -1,3 +1,17 @@
 //
 // Created by 李汇川 on 2022/5/29.
 //
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // Given that we know one child is a boy,
+  // we have the following sample space:
+  // (Child1=Boy, Child2=Girl) (Child1=Girl, Child2=Boy) (Child1=Boy, Child2=Boy)
+  // Thus the probability of both children being boys is 1/3.
+  cout << "1/3" << endl;
+
+  return 0;
+}
